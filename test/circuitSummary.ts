7f636ebb88724c49cb7bd0b57caf5a35eaaa37a2
jCabala <kuba.cabala6@gmail.com>
2025-03-09 15:58:21 +0000

    fix: Fixed import in test summary

--- test/circuitSummary.ts
+++ test/circuitSummary.ts
@@ -1,11 +1,13 @@
 import { ZkProgram, Field } from "o1js";
-import { IterativeAes128 } from "../src/implementations/IterativeAES128.js";
+import {
+  IterativeAes128,
+  IterativeAES128PublicInput as AESPublicInput,
+} from "../src/implementations/IterativeAES128.js";
 import { addRoundKey } from "../src/lib/AddRoundKey.js";
 import { mixColumn } from "../src/lib/MixColumns.js";
 import { sbox, sbox_byte } from "../src/lib/SBox.js";
 import { shiftRows } from "../src/lib/ShiftRows.js";
 import { Byte16 } from "../src/primitives/Bytes.js";
-import { AESPublicInput } from "../src/temp/run.js";
 import { expandKey128 } from "../src/lib/KeyExpansion.js";
 
 const libZkProgram = ZkProgram({
