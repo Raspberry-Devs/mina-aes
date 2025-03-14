4b834ca123c9603dca9354163406d45931b17d65
scaraven <sc4raven@gmail.com>
2025-03-14 19:11:22 +0000

    feat: add doc script to package.json

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {
+  IterativeAes128,
+  IterativeAES128PublicInput,
+} from "./implementations/IterativeAES128";
+import { Byte16 } from "./primitives/Bytes.js";
+
+export { IterativeAes128, IterativeAES128PublicInput, Byte16 };
