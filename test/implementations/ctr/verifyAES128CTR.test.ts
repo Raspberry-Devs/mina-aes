24c850aa9fff05f64e5546be3fa2ef56476895a3
scaraven <sc4raven@gmail.com>
2025-03-17 18:28:07 +0000

    test: implement test against fake zk proofs, need to find fix

--- test/implementations/ctr/verifyAES128CTR.test.ts
+++ test/implementations/ctr/verifyAES128CTR.test.ts
@@ -1,7 +1,9 @@
-import { Field, Poseidon, VerificationKey } from "o1js";
+import { Field, Poseidon, VerificationKey, ZkProgram } from "o1js";
 import { Byte16 } from "../../../src/primitives/Bytes";
+import { encryptAES128 } from "../../../src/utils/crypto";
 import {
   AES128HelperPublicInput,
+  CtrModeIterativeAes128Helper,
   SideLoadedAESProof,
   verifyIterativeCounterMode,
 } from "../../../src/implementations/ctr/IterativeAES128CTR";
@@ -22,6 +24,22 @@ function createDummyProof(
     },
   } as unknown as SideLoadedAESProof;
 }
+
+const fakeZKProgram = ZkProgram({
+  name: "aes-fake",
+  publicInput: AES128HelperPublicInput,
+
+  methods: {
+    verifyAESCTRHelper: {
+      privateInputs: [Byte16],
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async method(input: AES128HelperPublicInput, key: Byte16) {
+        Field(0).assertEquals(Field(0));
+      },
+    },
+  },
+});
 
 describe("verifyIterativeCounterMode", () => {
   it("verifies valid iterative counter mode proofs w dummy", () => {
@@ -139,6 +157,83 @@ describe("verifyIterativeCounterMode", () => {
     // will fail and verifyIterativeCounterMode should throw.
     expect(() =>
       verifyIterativeCounterMode(proofs, ciphers, messages, key, iv),
+    ).toThrow();
+  });
+
+  it("verifies valid iterative counter mode proofs", async () => {
+    const keyBytes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    const key = Byte16.fromBytes(keyBytes);
+    const hashKey = Poseidon.hashPacked(Byte16, key);
+    const iv = "00000000000000000000000000000001";
+
+    // Generate two proofs for two blocks.
+    const numBlocks = 2;
+    const proofs: SideLoadedAESProof[] = [];
+    const ciphers: Byte16[] = [];
+    const messages: Byte16[] = [
+      Byte16.fromHex("0123456789abcdef0123456789abcdef"),
+      Byte16.fromHex("fedcba9876543210fedcba9876543210"),
+    ];
+
+    for (let i = 0; i < numBlocks; i++) {
+      const counterField = Field(iv).add(Field(i));
+      const ctrInput = Byte16.fromField(counterField);
+
+      const cipherBytes = encryptAES128(ctrInput.toHex(), key.toHex());
+
+      const input = new AES128HelperPublicInput({
+        message: ctrInput,
+        hashed_key: hashKey,
+        cipher: Byte16.fromHex(cipherBytes),
+      });
+
+      // Generate proof
+      const { proof } = await CtrModeIterativeAes128Helper.verifyAESCTRHelper(
+        input,
+        key,
+      );
+
+      ciphers.push(Byte16.xor(Byte16.fromHex(cipherBytes), messages[i]));
+      proofs.push(SideLoadedAESProof.fromProof(proof));
+    }
+
+    expect(() =>
+      verifyIterativeCounterMode(proofs, ciphers, messages, key, Field(iv)),
+    ).not.toThrow();
+  });
+
+  it("fails on invalid iterative counter mode proofs", async () => {
+    const keyBytes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    const key = Byte16.fromBytes(keyBytes);
+    const hashKey = Poseidon.hashPacked(Byte16, key);
+    const iv = "00000000000000000000000000000001";
+
+    const messages: Byte16[] = [
+      Byte16.fromHex("0123456789abcdef0123456789abcdef"),
+    ];
+
+    const counterField = Field(iv).add(Field(0));
+    const ctrInput = Byte16.fromField(counterField);
+
+    const cipherBytes = encryptAES128(ctrInput.toHex(), key.toHex());
+
+    const input = new AES128HelperPublicInput({
+      message: ctrInput,
+      hashed_key: hashKey,
+      cipher: Byte16.fromHex(cipherBytes),
+    });
+
+    // Generate proof
+    await fakeZKProgram.compile();
+    const { proof } = await fakeZKProgram.verifyAESCTRHelper(input, key);
+
+    const cipher = Byte16.xor(Byte16.fromHex(cipherBytes), messages[0]);
+
+    const proofs = [SideLoadedAESProof.fromProof(proof)];
+    const ciphers = [cipher];
+
+    expect(() =>
+      verifyIterativeCounterMode(proofs, ciphers, messages, key, Field(iv)),
     ).toThrow();
   });
 });
